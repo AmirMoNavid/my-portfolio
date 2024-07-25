fcf40e8e7fbc71d1610cf33a73efcd864555cd10
AmirMoNavid <amyrmhmdnwyd@gmail.com>
2024-07-25 15:48:15 +0430

    update images

--- components/Avatar.js
+++ components/Avatar.js
@@ -5,7 +5,7 @@ const Avatar = () => {
   return (
     <div className="hidden xl:flex xl:max-w-none">
       <Image
-        src={"/avatar.png"}
+        src={"/nextjs-github-pages/avatar.png"}
         width={737}
         height={678}
         alt=""
